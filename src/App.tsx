71a521ab066b9ad0026c035841c228e20f0dff5b
luix gabriel <luisgasbriel@gmail.com>
2023-06-06 19:33:39 -0300

    feat: styling homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,48 @@
 import { PomodoroTimer } from './components/pomodoro-timer'
+import { Newspaper } from 'lucide-react'
 
 function App() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="flex h-[80%] w-[80%] items-center justify-center gap-8 ">
-        <div className="flex w-[50%] justify-center text-7xl font-bold text-white  ">
+    <div className="flex h-screen  items-center justify-center">
+      <div className="flex h-[90%] w-[90%] items-center justify-center gap-8 ">
+        <div className="flex min-w-[50%] justify-center text-7xl font-bold text-white  ">
           <h1 className="font-raleway font-extrabold">Pomodoro App</h1>
         </div>
         <div className="h-5/6 w-1 rounded-sm bg-white" />
-        <div className="flex w-2/5 flex-col justify-center bg-yellow-300">
-          <h1>Digite seu tempo de pomodor</h1>
-          <input type="text" placeholder="ok" />
+        <div className="flex min-w-[40%] flex-col justify-center">
+          <div className="mb-5 flex w-full gap-4  text-lg ">
+            <Newspaper color="white" />
+            <h1 className="font-raleway text-white">
+              Digite seu tempo de pomodoro
+            </h1>
+            <input
+              type="text"
+              className=":focus border-b-2 border-white bg-transparent text-white outline-none"
+            />
+          </div>
+          <div className="mb-5 flex gap-5 text-lg">
+            <Newspaper color="white" />
+            <h1 className="font-raleway text-white">
+              Digite seu tempo de pomodoro
+            </h1>
+            <input
+              type="text"
+              className=":focus border-b-2 border-white bg-transparent text-white outline-none"
+            />
+          </div>
+          <div className=" flex gap-5 text-lg">
+            <Newspaper color="white" />
+            <h1 className="font-raleway text-white">
+              Digite seu tempo de pomodoro
+            </h1>
+            <input
+              type="text"
+              className=":focus border-b-2 border-white bg-transparent text-white outline-none"
+            />
+          </div>
         </div>
       </div>
-      <h1>
-        <PomodoroTimer defaultPomodoroTimer={0} />
-      </h1>
+      <h1>{/* <PomodoroTimer defaultPomodoroTimer={0} /> */}</h1>
     </div>
   )
 }
