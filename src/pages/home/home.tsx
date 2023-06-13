3548a448de3d7c2cc64eee6b9ee8f19bab2c0868
luix gabriel <luisgasbriel@gmail.com>
2023-06-13 12:28:29 -0300

    feat: adding responsive

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -27,15 +27,15 @@ export function Home() {
   }
   return (
     <div className="flex h-screen  items-center justify-center">
-      <div className="items-center justify-center gap-8 md:flex md:h-[90%] md:w-[90%] ">
+      <div className="flex h-screen flex-col items-center justify-center gap-8 md:flex md:h-[90%] md:w-[90%] md:flex-row ">
         <div className="mb-5 flex min-w-[50%] justify-center text-5xl font-bold text-white md:text-7xl  ">
           <h1 className="font-raleway font-extrabold">Pomodoro App</h1>
         </div>
-        <div className="h-5/6 w-1 rounded-sm bg-white" />
-        <div className="flex flex-col justify-center md:min-w-[40%]">
-          <div className="my-1 flex w-full gap-4 text-lg md:mb-5">
+        <div className="hidden h-5/6 w-1 rounded-sm bg-white md:block" />
+        <div className="w-90 justify-centertext-sm flex flex-col sm:min-w-[40%]">
+          <div className="md:text-lg5 my-1 flex w-full gap-4 text-sm sm:my-0 sm:mb-5">
             <Newspaper color="white" />
-            <h1 className="font-raleway text-white">Task</h1>
+            <h1 className="text-md font-raleway text-white">Task</h1>
             <input
               type="text"
               className=":focus border-b-2 border-white bg-transparent text-white outline-none"
@@ -45,7 +45,7 @@ export function Home() {
               required
             />
           </div>
-          <div className="my-3 flex w-full gap-4 text-lg md:mb-5 ">
+          <div className="my-1 flex w-full gap-4 text-sm sm:my-0 sm:mb-5 md:text-lg ">
             <Timer color="white" />
             <h1 className="font-raleway text-white">Timer</h1>
             <input
@@ -57,7 +57,7 @@ export function Home() {
               required
             />
           </div>
-          <div className=" my-3 flex w-full gap-4 text-lg md:mb-5 ">
+          <div className=" my-1 flex w-full gap-4 text-sm sm:my-0 sm:mb-5 md:text-lg">
             <Coffee color="white" />
             <h1 className="font-raleway text-white">Short time to rest</h1>
             <input
@@ -69,7 +69,7 @@ export function Home() {
               required
             />
           </div>
-          <div className=" my-3 flex w-full gap-4 text-lg md:mb-5 ">
+          <div className=" my-1 flex w-full gap-4 text-sm sm:my-0 sm:mb-5 md:text-lg">
             <BedDouble color="white" />
             <h1 className="font-raleway text-white">Long time to rest</h1>
             <input
@@ -81,7 +81,7 @@ export function Home() {
               required
             />
           </div>
-          <div className="my-3 flex w-full gap-4 text-lg md:mb-5">
+          <div className="my-1 flex w-full gap-4 text-sm sm:my-0 sm:mb-5 md:text-lg">
             <ArrowUp10 color="white" />
             <h1 className="font-raleway text-white">Number of pomodoros</h1>
             <input
